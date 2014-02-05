ab55f5ebadbd43869ea721c4c438cedc92482cf9
rockerest <tom@vu>
2014-02-05 00:31:37 -0600

    Moved files to app-specific directories & renamed require

--- js/app/mochai/bootstrap.js
+++ js/app/mochai/bootstrap.js
@@ -1,11 +1,11 @@
 requirejs.config({
     "urlArgs": "_=" + (new Date()).getTime(),
     "paths":{
-        "lib":          "../lib",
+        "lib":          "../../lib",
 
-        "jquery":       ["//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min", "../lib/jquery/jquery.min"],
+        "jquery":       ["//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min", "../../lib/jquery/jquery.min"],
 
-        "sammy":        "../lib/sammy/lib/sammy"
+        "sammy":        "../../lib/sammy/lib/sammy"
     },
     "shim": {
         "sammy": {
